39f85e4b7f9cc9e211b0fbb509c5543525eaafb5
BeomYoonHeo <gqd0512@naver.com>
2022-10-05 17:43:37 +0900

    stomp 초기세팅 완료

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,24 @@
+var stompClient = null;
+
+
+function connect() {
+    var socket = new SockJS('/gs-guide-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+
