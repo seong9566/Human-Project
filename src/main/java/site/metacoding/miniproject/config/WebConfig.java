340ba1cb1d83788fbff45493ae5d3a73668cb054
BeomYoonHeo <gqd0512@naver.com>
2022-10-05 17:21:21 +0900

    web socket 의존성 추가 및 config파일 완료/주소 API 테스트 완료

--- src/main/java/site/metacoding/miniproject/config/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package site.metacoding.miniproject.config;
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-public class WebConfig implements WebMvcConfigurer{
-	
-	private String resourcePath;
-
-}
