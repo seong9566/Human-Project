e52d2313e6ebb96b344566e2b8351d4f0f4242b2
BeomYoonHeo <gqd0512@naver.com>
2022-10-14 06:14:37 +0900

    유저 중복로그인 방지 로직 구현 완료

--- src/main/java/site/metacoding/miniproject/config/SessionConfig.java
+++ src/main/java/site/metacoding/miniproject/config/SessionConfig.java
@@ -4,56 +4,30 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionAttributeListener;
-import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-import org.springframework.context.annotation.Configuration;
-
-import site.metacoding.miniproject.domain.users.Users;
-
 @WebListener
-public class SessionConfig implements HttpSessionListener, HttpSessionAttributeListener{
-	
-    private static final Map<String, HttpSession> sessions = new ConcurrentHashMap<>();
+public class SessionConfig implements HttpSessionListener {
 
-    //중복로그인 지우기
-    public synchronized static String getSessionidCheck(String type, Integer id){
-        String result = null;
-        System.out.println(sessions);
-        for( String key : sessions.keySet() ){
-            HttpSession value = sessions.get(key);
-            Users userinfo = (Users)value.getAttribute(type);
-            System.out.println(userinfo);
-//            if(value != null &&  userinfo != null && userinfo.getUsersId().equals(id) ){
-//                //System.out.println(value.getAttribute(type).toString());
-//                result =  key;
-//            	removeSessionForDoubleLogin(result);
-//            }
+    private static final Map<String, Integer> loginUserIdList = new ConcurrentHashMap<>();
+
+    public synchronized static Integer getSessionidCheck(Integer userinfo) {
+        Integer result = null;
+        for (String id : loginUserIdList.keySet()) {
+            if (loginUserIdList.get(id) == userinfo) {
+                result = userinfo;
+            }
         }
         return result;
     }
-    
-    private static void removeSessionForDoubleLogin(String userId){    	
-        System.out.println("remove userId : " + userId);
-        if(userId != null && userId.length() > 0){
-            sessions.get(userId).invalidate();
-            sessions.remove(userId);    		
-        }
+
+    public static void logout(String usersId) {
+        loginUserIdList.remove(usersId);
+
     }
 
-    @Override
-    public void sessionCreated(HttpSessionEvent hse) {
-        System.out.println(hse);
-        sessions.put(hse.getSession().getId(), hse.getSession());
+    public static void login(String usersInfo, Integer usersId) {
+        loginUserIdList.put(usersInfo, usersId);
     }
 
-    @Override
-    public void sessionDestroyed(HttpSessionEvent hse) {
-        if(sessions.get(hse.getSession().getId()) != null){
-            sessions.get(hse.getSession().getId()).invalidate();
-            sessions.remove(hse.getSession().getId());	
-        }
-    }
-}   
+}
