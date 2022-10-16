153057cc05cc99dbbba0c51440f4b0d1aa9fa774
BeomyoonHeo <55969155+BeomyoonHeo@users.noreply.github.com>
2022-10-16 15:36:13 +0900

    Alarmmanager (#22)
    [허범윤]이력서 좋아요 시 알람 추가

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -2,29 +2,16 @@
 var stompClient = null;
 
 
-$("#alarm").click(()=>{
-	let userid = $("#userId").val();
-	
-	$.ajax("/user/alarm/"+userid,{
-		type:"GET",
-		data:"JSON"
-	}).done((res)=>{
-		if(res.code == 1 && res.data != null){
-			$(".modal-body").empty();
-			res.data.forEach((object)=>{
-				$(".modal-body").append(`<p3>`+object.alarmMessage+`</p3>`);	
-			})
-			
-		}
-	});
+$("#alarm").click(() => {
+	refreshalarm();
 });
 
 function sendmessageToPersonal(data) {
-	stompClient.send("/app/Personal", {}, data);
+	stompClient.send("/app/Personal/" + $("#userId").val(), {}, data);
 }
 
 function sendmessageToCompany(data) {
-	stompClient.send("/app/Company", {}, data);
+	stompClient.send("/app/Company/", {}, data);
 }
 
 function connectpersonal() {
@@ -32,11 +19,16 @@ function connectpersonal() {
 	stompClient = Stomp.over(socket);
 	stompClient.connect({}, (frame) => {
 		//console.log('Connected: ' + frame);
-		stompClient.subscribe('/topic/Company', function(test) {
+		stompClient.subscribe('/topic/Company', function (test) {
 			let confirm = JSON.parse(test.body);
-			if(confirm.code == 1)
-			{
-				iconchange(confirm.data);
+			if (confirm.code == 1) {
+				iconchange();
+			}
+		});
+		stompClient.subscribe('/queue/Company', function (test) {
+			let confirm = JSON.parse(test.body);
+			if (confirm.code == 1) {
+				iconchange();
 			}
 		});
 	});
@@ -45,19 +37,26 @@ function connectpersonal() {
 function connectcompany() {
 	var socket = new SockJS('/company_end_point');
 	stompClient = Stomp.over(socket);
-	stompClient.connect({}, function(frame) {
+	stompClient.connect({}, function (frame) {
 		//console.log('Connected: ' + frame);
-		stompClient.subscribe('/topic/Personal', function(test){
+		stompClient.subscribe('/topic/Personal', function (test) {
 			let confirm = JSON.parse(test.body);
 			console.log(confirm);
-			if(confirm.code == 1)
-			{
-				iconchange(confirm.data);
+			if (confirm.code == 1) {
+				iconchange();
 			}
 
 		});
+		stompClient.subscribe('/queue/Personal', function (test) {
+			let confirm = JSON.parse(test.body);
+			if (confirm.code == 1) {
+				iconchange();
+			}
+		});
 	});
 }
+
+
 
 function disconnect() {
 	if (stompClient !== null) {
@@ -65,11 +64,41 @@ function disconnect() {
 	}
 }
 
+function refreshalarm() {
+	let userid = $("#userId").val();
 
-function iconchange(message){
+	$.ajax("/user/alarm/" + userid, {
+		type: "GET",
+		data: "JSON"
+	}).done((res) => {
+		if (res.code == 1 && res.data != null) {
+			$(".modal-body").empty();
+			res.data.forEach((object) => {
+				$(".modal-body").append(`<div style="display:inline-block; width:480px; height:70px;"><p3>` + object.alarmMessage + `</p3><span style="float:right"><button class="btn-close" type="button" onclick="deletealarm(` + object.alarmId + `)"></button></span></div>`);
+			})
+		} else {
+			$(".modal-body").empty();
+			$(".modal-body").append(`<h3>내게 온 알림이 없습니다.</h3>`);
+		}
+	});
+}
+
+function deletealarm(alarmId) {
+
+	$.ajax("/user/alarm/" + alarmId, {
+		type: "DELETE",
+		data: "JSON"
+	}).done((res) => {
+		if (res.code == 1) {
+			refreshalarm();
+		}
+	});
+
+}
+
+
+function iconchange() {
 	$("#alarm").removeClass("fa-regular");
 	$("#alarm").addClass("fa-solid");
 	$("#alarm").css("color", "red");
-	$(".modal-body").empty();
-	$(".modal-body").append('<p3>'+message+'</p3>');
 }
--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -25,6 +25,7 @@ $("#btnUsernameSameCheck").click(() => {
 	}
 });
 
+
 $("#passwordConfirm").keyup((event) => {
 	event.preventDefault();
 	if ($("#password").val() != $("#passwordConfirm").val()) {
@@ -58,14 +59,37 @@ $("#btnSave").click(() => {
 		}
 	});
 	
+	//주소불러오기
+function findAddr() {
+	new daum.Postcode(
+		{
+			oncomplete: function(data) {
+				console.log(data);
+				// 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+				// 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
+				// 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+				var roadAddr = data.roadAddress; // 도로명 주소 변수
+				var jibunAddr = data.jibunAddress; // 지번 주소 변수
+				// 우편번호와 주소 정보를 해당 필드에 넣는다.
+				document.getElementById('post').value = data.zonecode;
+				if (roadAddr !== '') {
+					document.getElementById("addr").value = roadAddr;
+				} else if (jibunAddr !== '') {
+					document.getElementById("addr").value = jibunAddr;
+				}
+			}
+		}).open();
+}
+
+	
 	   //유효성겁사
 	   function joinform_check() {
 	        //변수에 담아주기
 	        var userId = document.getElementById("userId");
 	        var password = document.getElementById("password");
 	        var username = document.getElementById("username");
-	        var phonenumber = document.getElementById("phonenumber");
-	        var email = $("#email").val();     
+	        let phonenumber = document.getElementById("phonenumber");
+	        let email = $("#email").val();     
 	     
 	        //아이디 입력하지 않았을때
 	        if ((userId.value) == ""){
