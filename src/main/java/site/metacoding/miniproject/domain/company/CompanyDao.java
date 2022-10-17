825e90dac79610a0ff5232159697bf0fcd9e10e0
donghun-park-code <sopu55544@gmail.com>
2022-10-17 17:48:41 +0900

    좋아요 완료

--- src/main/java/site/metacoding/miniproject/domain/company/CompanyDao.java
+++ src/main/java/site/metacoding/miniproject/domain/company/CompanyDao.java
@@ -7,16 +7,17 @@ import site.metacoding.miniproject.web.dto.response.CompanyInfoDto;
 
 public interface CompanyDao {
 	public void insert(Company company);
+
 	public List<Company> findAll();
+
 	public Company findById(Integer companyId);
-	
+
 	public void update(Company company);
-	
+
 	public void deleteById(Integer id);
-	
+
 	public CompanyInfoDto companyInfo(Integer companyId);
+
 	public CompanyAddressDto findByAddress(Integer companyId);
-	
-	
 
 }
